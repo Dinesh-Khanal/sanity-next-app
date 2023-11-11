2be95dd5d078797d0d3ecbc5109539a38eb1bff9
Dinesh-Khanal <dinesh_Khanal@hotmail.com>
2023-11-11 22:21:27 +0545

    some styling done in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,28 @@
 import { getProjects } from "@/sanity/sanity-utils";
+import Link from "next/link";
 
 export default async function Home() {
   const projects = await getProjects();
   return (
-    <main className="flex min-h-screen flex-col py-4 px-12">
-      <h2 className="text-3xl">This is my home page</h2>
-      {projects.map((project) => (
-        <div key={project._id}>{project.name}</div>
-      ))}
+    <main className=" py-4 px-12">
+      <h1 className="text-7xl">
+        Hello ! I am{" "}
+        <span className="bg-gradient-to-r from-orange-500 via-red-800 to-purple-600 bg-clip-text text-transparent">
+          Dinesh
+        </span>
+      </h1>
+      <h2 className="text-3xl font-bold mt-8">My Project</h2>
+      <div className="mt-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 !gap-8">
+        {projects.map((project) => (
+          <Link
+            href={`/project/${project.slug}`}
+            key={project._id}
+            className="p-4 border border-red-900"
+          >
+            {project.name}
+          </Link>
+        ))}
+      </div>
     </main>
   );
 }
